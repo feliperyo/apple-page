e3463b25eb57300c2e2c9a58d26e54d593ac14e4
feliperyo <ryofeliperyo@gmail.com>
2024-05-08 11:23:24 -0300

    adicionando scripts

---
+++ scripts.js
@@ -0,0 +1,10 @@
+const circle = document.querySelector('.circulo')
+const img = document.querySelector('.cell-img')
+
+function changeColor(color) {
+    circle.style.backgroundColor = color
+}
+
+function changeImg(image) {
+    img.src = image
+}
